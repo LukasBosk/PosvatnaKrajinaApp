761e00534f27fbbf22ff2bbe73715a9966770ca4
LukasBosk <bedan@muzeum-boskovicka.cz>
2025-09-16 13:43:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muzeum-boskovicka-cache-v86';
+const CACHE_NAME = 'muzeum-boskovicka-cache-v87';
 
 // Seznam souborů, které se mají při instalaci Service Workeru uložit do cache
 // DŮLEŽITÉ: Ujistěte se, že zde jsou uvedeny VŠECHNY soubory, které aplikace potřebuje k offline provozu,
@@ -155,6 +155,7 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+
 
 
 
